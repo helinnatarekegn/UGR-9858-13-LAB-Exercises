900240b8ebc73995944dc133bed899ac09229ba7
Helina <helinatarekegn1213@gmail.com>
2024-03-08 17:40:01 +0300

    commit

---
+++ LAB-12-Classes-and-Objects.dart
@@ -0,0 +1,25 @@
+class Rectangle {
+  double width;
+  double height;
+
+  Rectangle(this.width, this.height);
+
+  double calculateArea() {
+    return width * height;
+  }
+
+  double calculatePerimeter() {
+    return 2 * (width + height);
+  }
+}
+
+void main() {
+  
+  Rectangle rectangle = Rectangle(5.0, 3.0);
+
+  double area = rectangle.calculateArea();
+  double perimeter = rectangle.calculatePerimeter();
+
+  print("Area: $area");
+  print("Perimeter: $perimeter");
+}
