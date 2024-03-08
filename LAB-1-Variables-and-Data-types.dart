7df07bdd50b0e90428a4d14f94a7cb99739d730c
Helina <helinatarekegn1213@gmail.com>
2024-03-08 15:24:45 +0300

    commit

---
+++ LAB-1-Variables-and-Data-types.dart
@@ -0,0 +1,7 @@
+void main() {
+  String name = "Helina";
+  int age = 22;
+  String favoriteColor = "Brown";
+  
+  print("My name is $name, I am $age years old, and my favorite color is $favoriteColor.");
+}
